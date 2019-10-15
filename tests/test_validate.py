e182ca183d52d8c8a4a2286c9b09fa5b539629ce
Ross Reedstrom <reedstrm@gmail.com>
2019-10-15 15:15:28 -0500

    a few more tests, for coverage

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -5,7 +5,7 @@ from urllib.parse import urlencode
 from validator import app
 
 
-os.environ["VALIDATOR_SETTINGS"] = '../tests/testing.cfg'
+os.environ["VALIDATOR_SETTINGS"] = "../tests/testing.cfg"
 
 myapp = app.create_app()
 PARSER_DEFAULTS = myapp.config["PARSER_DEFAULTS"]
@@ -36,6 +36,31 @@ def client():
     myapp.config["TESTING"] = True
     client = myapp.test_client()
     yield client
+
+
+def test_validate_response():
+    from validator.validate_api import validate_response
+
+    res = validate_response("foo bar", "100@1", {})
+    expected = {
+        "inner_product": 0,
+        "intercept": 1,
+        "lazy_math_evaluation": True,
+        "num_spelling_correction": 0,
+        "processed_response": "foo bar",
+        "remove_nonwords": True,
+        "remove_stopwords": True,
+        "response": "foo bar",
+        "spelling_correction": "auto",
+        "spelling_correction_used": True,
+        "tag_numeric": "auto",
+        "tag_numeric_input": "auto",
+        "uid_found": True,
+        "uid_used": "100@7",
+        "valid": False,
+    }
+
+    assert res == expected
 
 
 def test_404(client):
